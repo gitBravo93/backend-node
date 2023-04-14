1db53d9a40cef00634fd9b431a84e937b3977475
gitBravo93 <douglasbravo1993@mail.com>
2023-04-14 12:01:51 -0400

    Configurando consultas

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -71,12 +71,8 @@ export const deleteBlog = async (req, res) => {
     try {
       const { id } = req.params;
       const [rows] = await pool.query("DELETE FROM blogs WHERE id = ?", [id]);
-  
-      if (rows.affectedRows <= 0) {
-        return res.status(404).json({ message: "blogs not found" });
-      }
-  
       res.sendStatus(204);
+      
     } catch (error) {
       return res.status(500).json({ message: "Something goes wrong" });
     }
