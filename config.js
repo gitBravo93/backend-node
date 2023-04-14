de6e251ae20f14efcf9b34cdc5ee1f8d44e52a3e
gitBravo93 <douglasbravo1993@mail.com>
2023-04-14 16:35:16 -0400

    api actualizada

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ import { config } from 'dotenv'
 
 config()
 
-// export const PORT = process.env.PORT || 8000
-// export const DB_HOST = process.env.DB_HOST || 'localhost'
-// export const DB_PORT = process.env.DB_POST || 3306
-// export const DB_USER = process.env.DB_USER || 'root'
-// export const DB_PASSWORD = process.env.DB_PASSWORD || ''
-// export const DB_DATABASE = process.env.DB_DATABASE || 'database_app'
-
 export const PORT = process.env.PORT || 8000
-export const DB_HOST = process.env.DB_HOST || 'mysql-toro.alwaysdata.net'
+export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PORT = process.env.DB_POST || 3306
-export const DB_USER = process.env.DB_USER || 'toro'
-export const DB_PASSWORD = process.env.DB_PASSWORD || '#Torodata93'
-export const DB_DATABASE = process.env.DB_DATABASE || 'toro_database_app'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || ''
+export const DB_DATABASE = process.env.DB_DATABASE || 'database_app'
+
+// export const PORT = process.env.PORT || 8000
+// export const DB_HOST = process.env.DB_HOST || 'mysql-toro.alwaysdata.net'
+// export const DB_PORT = process.env.DB_POST || 3306
+// export const DB_USER = process.env.DB_USER || 'toro'
+// export const DB_PASSWORD = process.env.DB_PASSWORD || '#Torodata93'
+// export const DB_DATABASE = process.env.DB_DATABASE || 'toro_database_app'
