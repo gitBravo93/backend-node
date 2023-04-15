37e79e13f4317704defb58142f9fa7b1b355d18d
gitBravo93 <douglasbravo1993@mail.com>
2023-04-15 15:20:07 -0400

    ORDER BY

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -6,7 +6,7 @@ import { pool } from "../database/db.js";
 
 export const getAllBlogs = async (req, res) => {
     try {
-      const [rows] = await pool.query("SELECT * FROM blogs");
+      const [rows] = await pool.query("SELECT * FROM blogs ORDER BY id desc");
       res.json(rows);
     } catch (error) {
       return res.status(500).json({ message: "Something goes wrong" });
